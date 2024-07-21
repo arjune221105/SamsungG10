ecca2fc45155e8cb60127584c915a55225edfdfc
arjune221105 <shakti221105@gmail.com>
2024-07-20 22:24:28 -0700

    add

---
+++ Samsungshop/src/test/java/Owner/ToStorPlace.java
@@ -0,0 +1,10 @@
+package Owner;
+
+public class ToStorPlace {
+
+	public static void main(String[] args) {
+		System.out.println("To Stor Place");
+		System.out.println("Banglor,Chennei,Hydbrad,Varanasi,Bihar");
+		System.out.println("All there Place Stor Successfully Done");
+	}
+}
---
+++ Samsungshop/src/test/java/State/ToCreateStor_State.java
@@ -0,0 +1,10 @@
+package State;
+
+public class ToCreateStor_State {
+
+	public static void main(String[] args) {
+		System.out.println("Lakhnow");
+		System.out.println("Mumbai");
+	}
+
+}
